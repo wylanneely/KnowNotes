9d412597f7350b8f26c7235a4ba60cec68de21a9
wylanneely <wylanneely@gmail.com>
2017-07-21 11:19:25 -0600

    Added collection view to settings

---
+++ Know Notes/Know Notes/Views/SoundsCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SoundsCollectionViewCell.swift
+//  Know Notes
+//
+//  Created by ALIA M NEELY on 7/21/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import UIKit
+
+class SoundsCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var soundImageView: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+}
