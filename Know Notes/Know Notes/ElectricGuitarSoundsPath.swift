c22d1d697b7ccf76755f26c20d831b0d8c40d827
wylanneely <wylanneely@gmail.com>
2017-07-20 22:48:29 -0600

    Set up Unlock mode

---
+++ Know Notes/Know Notes/ElectricGuitarSoundsPath.swift
@@ -0,0 +1,21 @@
+//
+//  ElectricGuitarSoundsPath.swift
+//  Know Notes
+//
+//  Created by ALIA M NEELY on 7/20/17.
+//  Copyright © 2017 Wylan. All rights reserved.
+//
+
+import Foundation
+
+struct ElectricSoundsPath {
+    
+    let gSoundPath = Bundle.main.path(forResource: "ElectricGuitarG", ofType: "wav")
+    let fSoundPath = Bundle.main.path(forResource: "ElectricGuitarF", ofType: "wav")
+    let eSoundPath = Bundle.main.path(forResource: "ElectricGuitarE", ofType: "wav")
+    let dSoundPath = Bundle.main.path(forResource: "ElectricGuitarD", ofType: "wav")
+    let cSoundPath = Bundle.main.path(forResource: "ElectricGuitarC", ofType: "wav")
+    let bSoundPath = Bundle.main.path(forResource: "ElectricGuitarB", ofType: "wav")
+    let ASoundPath = Bundle.main.path(forResource: "ElectricGuitarA", ofType: "wav")
+    
+}
