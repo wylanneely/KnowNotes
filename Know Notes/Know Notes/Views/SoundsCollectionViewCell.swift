78f82b1424ddc70d4885f88253ba259c8b355a87
wylanneely <wylanneely@gmail.com>
2017-08-02 14:22:22 -0600

    changed the buttons
    add buy sounds

--- Know Notes/Know Notes/Views/SoundsCollectionViewCell.swift
+++ Know Notes/Know Notes/Views/SoundsCollectionViewCell.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class SoundsCollectionViewCell: UICollectionViewCell {
     
-    var cellIndexPath: Int = 0
+    var isButtonSelected: Bool = false
     
     @IBOutlet weak var soundButton: UIButton!
+    @IBOutlet weak var selectedImageState: UIImageView!
+    @IBOutlet weak var normalImageState: UIImageView!
+    
+    func switchButtonStates(){
+        
+        if selectedImageState.isHidden == false {
+            selectedImageState.isHidden = true
+        }else if selectedImageState.isHidden == true {
+            selectedImageState.isHidden = false
+        }
+        
+        if normalImageState.isHidden == true {
+           normalImageState.isHidden = false
+        } else if normalImageState.isHidden == false {
+            normalImageState.isHidden = true
+        }
+
+        
+    }
+    
+    var cellIndexPath: Int = 0
+   
+    
     @IBAction func soundButtonTapped(_ sender: Any) {
         delegate?.settingButtonChanges(cell: self, selectedValue: cellIndexPath)
-        
+        switchButtonStates()
     }
     
     weak var delegate: SoundCollectioncellDelegate?
